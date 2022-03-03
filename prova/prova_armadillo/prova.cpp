26aaa22e9ce1cb7f2f8492693f081af72732de84
RandomPhile <richifurlan@gmail.com>
2022-03-03 11:34:40 +0100

    prova con armadillo
    eseguire con
    g++ file.cpp -std=c++1y -larmadillo -o prova
    dopo aver installato la libreria armadillo

---
+++ prova/prova_armadillo/prova.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//#include <armadillo>
+//eseguire con g++ file.cpp -std=c++1y -larmadillo -o prova
+
+using namespace std;
+
+int main() {
+	char stringa[] = "CIAO21";
+	cout<<stringa<<endl;
+	return 0;
+}
