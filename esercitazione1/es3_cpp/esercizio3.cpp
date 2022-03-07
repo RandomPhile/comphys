14e16a70f3952412ee4654929e214985f0b7bfc3
elericon <100796314+elericon@users.noreply.github.com>
2022-03-07 19:06:32 +0100

    Update esercizio3.cpp
    sistemati errori segnalati

--- esercitazione1/es3_cpp/esercizio3.cpp
+++ esercitazione1/es3_cpp/esercizio3.cpp
@@ -4,31 +4,32 @@
 using namespace std;
 
 int main() {
-    float a=1, b, c=1, x1, x2, x1c;
-    double ad, bd, cd, x1d, x1dc, x2d, err_rel1, err_rel2;
-    ad=a;//stai assegnando il valore di un float ad un double, sarebbe meglio fare viceversa se proprio
-    b=0;//questo valore di b non viene usato. era un rimasuglio di una cosa che stavo pensando
-    bd=b;
-    cd=c;
+    float a, b, c, x1, x2, x1c;
+    double ad=1, bd, cd=1, x1d, x1dc, x2d, err_rel1, err_rel2;
+    a=ad;
+    bd=1;//per avere una inizializzazione, poi inutile
+    b=bd;
+    c=cd;
     
     
     
-    for(int i=3; i<10000000;i=i*1.4){//i è un intero e gli assegni il valore di i*1.4. fair enough
-        b=i;//valore di un intero assegnato ad un float
-        bd=i;//valore di un intero ad un double
+    for(double i=3; i<10000000;i=i*1.4l){
+        b=i;
+        bd=i;
         printf("%f\n", b);
-        x1=(-b+sqrt(b*b-4*a*c))/(2*a);//mancano parentesi prima di -b grazie
-        x1c=2*c/(-b-sqrt(b*b-4*a*c));
-        x2=(-b-sqrt(b*b-4*a*c))/(2*a);//la soluzione x2 non viene mai usata... invece è quella interessante perchè è stabile
         
-        x1d=-bd+sqrt(pow(bd,2)-4*ad*cd);
+        x1=(-b+sqrt(b*b-4*a*c))/(2*a);//guardo la prima soluzione perche instabile e valuto l'errore per b grande
+        x1c=2*c/(-b-sqrt(b*b-4*a*c));
+        x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+        
+        x1d=-bd+sqrt(pow(bd,2)-4*ad*cd);//controllo la soluzione 1 con la versione stabile e controllo l'errore
         x1dc=2*cd/(-bd-sqrt(pow(bd,2)-4*ad*cd));
-        x2d=-bd-sqrt(pow(bd,2)-4*ad*cd);//qua immagino fosse x2d
+        x2d=(-bd-sqrt(pow(bd,2)-4*ad*cd))/(2*ad);
         
         
         err_rel1=fabs((x1d-x1)/x1d)/(2*ad);
         err_rel2=fabs((x1dc-x1c)/x1dc)/(2*ad);
-        //manca appunto la soluzione x2... ci interessa quella instabile proprio perche tale
+    
         cout<<i<<"\t"<<err_rel1<<"\t"<<err_rel2<<endl;
     }
     return 0;
