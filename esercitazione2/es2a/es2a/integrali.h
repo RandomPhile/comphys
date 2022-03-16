20a56af8e9cb0441d71b1b6566824ce5bd24f4d9
elericon <100796314+elericon@users.noreply.github.com>
2022-03-16 16:28:34 +0100

    Update integrali.h

--- esercitazione2/es2a/es2a/integrali.h
+++ esercitazione2/es2a/es2a/integrali.h
@@ -12,6 +12,18 @@ double simpson(double (*f)(double, double), double a, double b, int N, double pa
     I = (I - 2 * f(b, param)) * h / 3;
     return I;
 }
+double simpson(double (*f)(double), double a, double b, int N, double param) {
+    double h = (b - a) / N;
+    //cout<<h<<endl;
+    double I;
+    I = (f(a*param) + f(b*param));
+    for (int n = 1; n <= N / 2; n++) {
+        I += 2 * f((a + 2 * n * h)*param) + 4 * f((a + (2 * n - 1) * h)* param);
+    }
+    //tolgo l'elemento N/2 della prima sommatoria
+    I = (I - 2 * f(b*param)) * h / 3;
+    return I;
+}
 double trapezi(double (*f)(double, double), double a, double b, int N, double param) {
     double h = (b - a) / N;
 
@@ -19,6 +31,17 @@ double trapezi(double (*f)(double, double), double a, double b, int N, double pa
     I = (f(a, param) + f(b, param)) / 2;
     for (int i = 1; i < N; i++) {
         I += f(a + i * h, param);
+    }
+    I *= h;
+    return I;
+}
+double trapezi(double (*f)(double), double a, double b, int N, double param) {
+    double h = (b - a) / N;
+
+    double I;
+    I = (f(a*param) + f(b*param)) / 2;
+    for (int i = 1; i < N; i++) {
+        I += f((a + i * h)*param);
     }
     I *= h;
     return I;
