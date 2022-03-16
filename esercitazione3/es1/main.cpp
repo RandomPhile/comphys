5b3780edaaca1122a255fbe3166f83dbd3b93e0b
Riccardo Furlan <riccardo@Riccardos-MacBook-Air.local>
2022-03-16 17:03:47 +0100

    es1

--- esercitazione3/es1/main.cpp
+++ esercitazione3/es1/main.cpp
@@ -1,12 +1,49 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+ofstream dati;
+//putatore NULL
+double f(double t, double x, double y, double *fargs);
+double g(double t, double x, double y, double *gargs);
 
-int eulero_exp();
+int eulero_exp(double t, double *x, double *y, double h,
+               double (*f)(double,double,double,double*),
+               double *fargs,
+               double (*g)(double,double,double,double*),
+               double *gargs);
+
 int main() {
+    dati.open("dati.dat");
+    double t ,t0 = 0, t1 = 25, h, x=1, y=0;
+    int N = 100;
+    h = (t1-t0)/( (double) N);
+    printf("t\t\t\tx\t\t\ty\n");
 
+    for (int n = 0; n < N; n++) {
+        t = t0 + n*h;
+        if (eulero_exp(t, &x, &y, h, f, NULL, g, NULL)){printf("ERRORE");}
+
+        printf("%f\t%f\t%f\t\n", t,x,y);
+        dati<<t<<"\t"<<x<<"\t"<<t<<endl;
+    }
+    dati.close();
     return 0;
 }
 
-int eulero_exp(){
-    
+double f(double t, double x, double y, double *fargs){
+    return y;
+};
+double g(double t, double x, double y, double *gargs){
+    return -x;
+};
+
+int eulero_exp(double t, double *x, double *y, double h,
+               double (*f)(double,double,double,double*),
+               double *fargs,
+               double (*g)(double,double,double,double*),
+               double *gargs){
+    *x += h*f(t,*x,*y,NULL);
+    *y += h*g(t,*x,*y,NULL);
+
+    return 0;
 }
---
+++ esercitazione3/es1/plot.plt
@@ -0,0 +1,7 @@
+set term qt font "Helvetica" position 0,0
+set datafile sep '\t'
+set grid
+
+#set multiplot layout 1,2
+plot "dati.dat" u 1:2 w l
+pause -1
