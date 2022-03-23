f36ae8d60e29125bab1c10bd1652ecee7346ef1a
elericon <100796314+elericon@users.noreply.github.com>
2022-03-23 18:26:25 +0100

    ok

--- esercitazione4/es1/main.cpp
+++ esercitazione4/es1/main.cpp
@@ -9,23 +9,64 @@ double g(double r, double P, double m, double *gargs);
 
 int main() {
     dati.open("dati.dat");
-    double r,r0 = 0.01, h, m = 0, E, T;
+    double r,r0 = 0.01, h, m = 0, rho;
     double P = 1;
 
-    double gam = 5.0 / 3, K = 0.05; //gas fermi non rel
+    double gam= 5.0 / 3, K = 0.05;; //gas fermi non rel
     double args[] = {gam, K};
 
-
-    h = 1e-3;
+    h = 1e-5;
+    
     int i = 0;
     while (P > 0) {
         r = r0 + i * h;
         if (runge_kutta(r, &P, &m, h, f, args, g, args)) {printf("ERRORE");}
-        dati << r << "\t" << P << "\t" << m << endl;
+        rho=pow(P / (args[1] * (args[0] - 1)), 1 / args[0]);
+        dati << r << "\t" << P << "\t" << m << "\t"<<rho<<endl;
         i++;
     }
     printf("R = %f\n", r);
-
+    dati<<"\n\n";
+    
+    r0 = 0.01;
+    P=1;
+    m=0;
+    
+    gam=4/3;
+    K=0.1;
+    args[0]=gam;
+    args[1]=K;
+    i = 0;
+    while (P > 0) {
+        r = r0 + i * h;
+        if (runge_kutta(r, &P, &m, h, f, args, g, args)) {printf("ERRORE");}
+        rho=pow(P / (args[1] * (args[0] - 1)), 1 / args[0]);
+        dati << r << "\t" << P << "\t" << m << "\t"<<rho<<endl;
+        i++;
+    }
+    printf("R = %f\n", r);
+    dati<<"\n\n";
+    
+    
+    r0 = 0.01;
+    P=1;
+    m=0;
+    
+    gam=2.54;
+    K=0.01;
+    args[0]=gam;
+    args[1]=K;
+    i = 0;
+    while (P > 0) {
+        r = r0 + i * h;
+        if (runge_kutta(r, &P, &m, h, f, args, g, args)) {printf("ERRORE");}
+        rho=pow(P / (args[1] * (args[0] - 1)), 1 / args[0]);
+        dati << r << "\t" << P << "\t" << m << "\t"<<rho<<endl;
+        i++;
+    }
+    printf("R = %f\n", r);
+    dati<<"\n\n";
+    
     dati.close();
     return 0;
 }
--- esercitazione4/es1/plot.plt
+++ esercitazione4/es1/plot.plt
@@ -3,11 +3,19 @@ set datafile sep '\t'
 set grid
 
 #set multiplot layout 1,2
-set term qt 0 title "P"
-plot "dati.dat" i 0 u 1:2 w l
+set term qt 0 title "P" position 0,0
+plot 	"dati.dat" i 0 u 1:2 w l, \
+	"dati.dat" i 1 u 1:2 w l, \
+	"dati.dat" i 2 u 1:2 w l
 
-set term qt 1 title "m"
-plot "dati.dat" i 0 u 1:3 w l
+set term qt 1 title "m" position 800,0
+plot 	"dati.dat" i 0 u 1:3 w l, \
+	"dati.dat" i 1 u 1:3 w l, \
+	"dati.dat" i 2 u 1:3 w l
 
+set term qt 2 title "rho" position 400,350
+plot 	"dati.dat" i 0 u 1:4 w l, \
+	"dati.dat" i 1 u 1:4 w l, \
+	"dati.dat" i 2 u 1:4 w l
 
 pause -1
