90105a6871d4c6ca7b325c5cc5e0b2bbc537b1be
elericon <100796314+elericon@users.noreply.github.com>
2022-03-29 12:01:23 +0200

    correzione errori

--- esercitazione4/es1/main.cpp
+++ esercitazione4/es1/main.cpp
@@ -9,12 +9,12 @@ double g(double r, double P, double m, double *gargs);
 
 int main() {
     dati.open("dati.dat");
-    int N=3;
-    double r=0,r0 = 1e-5, h, m = 0, rho, P0=1;
+    int N=15;
+    double r=0,r0 = 1e-10, h, m = 1e-6, rho, P0=5;
     double M[N][3], R[N][3];
     h = 1e-5;
     for(int j=0; j<N; j++){
-        P0=j+1;
+        P0=j*4+P0;
         double P = P0;
 
         double gam= 5.0 / 3, K = 0.05;; //gas fermi non rel
@@ -27,13 +27,13 @@ int main() {
         
         int i = 0;
         while (P > 0) {
+            if(m!=NAN)
+                M[j][0]=m;
             r = r0 + i * h;
             if (runge_kutta(r, &P, &m, h, f, args, g, args)) {printf("ERRORE");}
             rho=pow(P / (args[1] * (args[0] - 1)), 1 / args[0]);
             dati << r << "\t" << P << "\t" << m << "\t"<<rho<<endl;
             i++;
-            if(m!=NAN)
-                M[j][0]=m;
         }
         R[j][0]=r;
         //printf("R = %f\n", r);
@@ -49,13 +49,13 @@ int main() {
         args[1]=K;
         i = 0;
         while (P > 0) {
+            if(m!=NAN)
+                M[j][1]=m;
             r = r0 + i * h;
             if (runge_kutta(r, &P, &m, h, f, args, g, args)) {printf("ERRORE");}
             rho=pow(P / (args[1] * (args[0] - 1)), 1 / args[0]);
             dati << r << "\t" << P << "\t" << m << "\t"<<rho<<endl;
             i++;
-            if(m!=NAN)
-                M[j][1]=m;
         }
         R[j][1]=r;
         //printf("R = %f\n", r);
@@ -72,25 +72,28 @@ int main() {
         args[1]=K;
         i = 0;
         while (P > 0) {
+            if(m!=NAN)
+                M[j][2]=m;
             r = r0 + i * h;
             if (runge_kutta(r, &P, &m, h, f, args, g, args)) {printf("ERRORE");}
             rho=pow(P / (args[1] * (args[0] - 1)), 1 / args[0]);
             dati << r << "\t" << P << "\t" << m << "\t"<<rho<<endl;
             i++;
-            if(m!=NAN)
-                M[j][0]=m;
         }
         R[j][2]=r;
         //printf("R = %f\n", r);
         dati<<"\n\n";
-        
+    
     }
     for(int t=0; t<3; t++){
         for(int j=0; j<N; j++){
             dati<<M[j][t]<<"\t"<<R[j][t]<<endl;
             cout<<M[j][t]<<"\t"<<R[j][t]<<endl;
         }
-        dati<<"\n\n";
+        if(t<2){
+            cout<<"\n\n";
+            dati<<"\n\n";
+        }
     }
     dati.close();
     return 0;
--- esercitazione4/es1/plot.plt
+++ esercitazione4/es1/plot.plt
@@ -1,6 +1,7 @@
 #set term qt font "Helvetica" position 0,0
 set datafile sep '\t'
 set grid
+stats 'dati.dat' u 1
 
 #set multiplot layout 1,2
 set term qt 0 title "P" position 0,0
@@ -19,9 +20,11 @@ plot 	"dati.dat" i 0 u 1:4 w l lt rgb "violet", \
 	"dati.dat" i 2 u 1:4 w l
 
 set term qt 3 title "M-R" position 400,350
-plot 	"dati.dat" i 5 u 1:2 w p
-	#"dati.dat" i 3 u 1:2  w p lt rgb "violet", \
-	#"dati.dat" i 4 u 1:2  w p lt rgb "red", \
+plot 	"dati.dat" i STATS_blocks-3 u 1:2  w p lt rgb "violet", \
+	"dati.dat" i STATS_blocks-2 u 1:2  w p lt rgb "red", \
+	"dati.dat" i STATS_blocks-1 u 1:2 w p
 	
+
+#plot 'file.dat' i STATS_blocks-1 usato per accedere all'ultimo blocco
 
 pause -1
