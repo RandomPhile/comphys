6ecec439c04a8073eb3e97e85222888858007ad4
RandomPhile <richifurlan@gmail.com>
2022-04-05 18:07:57 +0200

    fin

--- da_consegnare/esercitazione2/es2/main.cpp
+++ da_consegnare/esercitazione2/es2/main.cpp
@@ -22,7 +22,7 @@ int main() {
     double Mpc2 = 938.272;//MeV
     double muc2 = Mnc2 * Mpc2 / (Mnc2 + Mpc2);
     double lam = pow(hc, 2) / (2 * muc2 * pow(R, 2));
-    
+
     v = 2 * V0 * muc2 * pow(R, 2) / pow(hc, 2);
     printf("v = %f\n\n", v);
 
@@ -47,16 +47,13 @@ int main() {
     double k = sqrt(2 * muc2 * (V0 + E) / pow(hc, 2));
     double q = sqrt(2 * muc2 * fabs(E) / pow(hc, 2));
 
-
-
-
     double args[] = {0, R, k, 38};//integrale da 0 a R con parametro k ed N = 38
-    
+
     double I1 = simpson(f2, args);
     double I3 = simpson(f1, args);
 
     args[2] = 2 * q;//cambiamo parametro con 2q
-    
+
     //calcolo integrali indefiniti e moltiplico per coefficente B/A
     double B_A = exp(2 * q * R) * pow(sin(k * R), 2);
     double I2 = B_A * (2 / pow(2 * q, 3) - simpson(f4, args));
@@ -67,5 +64,3 @@ int main() {
     dati.close();
     return 0;
 }
-
-
