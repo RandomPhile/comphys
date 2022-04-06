524e6a274310a4f0ea7373579a9c32dfcb44bdf5
elericon <100796314+elericon@users.noreply.github.com>
2022-04-06 10:57:02 +0200

    correzioni relativistiche non funzionante

--- esercitazione4/es1/main.cpp
+++ esercitazione4/es1/main.cpp
@@ -6,6 +6,8 @@ ofstream dati, dati1;
 
 double f(double r, double P, double m, double *fargs);
 double g(double r, double P, double m, double *gargs);
+double dm(double r, double P, double m, double *fargs);
+double dP(double r, double P, double m, double *fargs);
 
 int main() {
     dati.open("dati.dat"); dati1.open("dati1.dat");
@@ -13,7 +15,7 @@ int main() {
     double K[] = {0.05, 0.1, 0.01};
     double gam[] = {5.0 / 3, 4.0 / 3, 2.54};
 
-    double m, P, r, rho, args[2], cost;
+    double m, P, r, rho, args[2], cost, e;
     double r0 = 0.00001, h = 1e-3;
     // double Pc_min[] = {5,5,5};
     // double Pc_max[] = {10,10,10};
@@ -21,11 +23,11 @@ int main() {
     double Pc_min[] = {6e-6, 3e-2, 4e-7};
     double Pc_max[] = {1e7, 3e3, 2e5};
 
-    int N = 1000;//da scrivere anche su plot.plt
+    int N = 100;//da scrivere anche su plot.plt
     double R[3][N], M[3][N], Pc[3][N];
 
     double R0 = 20.06145;//km
-    for (int i = 0; i < 3; ++i) {
+    /*for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < N; ++j) {
             dati << "\n\n(i,j) = (" << i << "," << j << ")" << endl;
             //dati << "r\tP\tm\trho" << endl;
@@ -54,7 +56,49 @@ int main() {
             }
         }
     }
+*/
+    
+    
+    //calcolo ora i risultati per le stelle relativistiche
+    double Pc_min_rel[] = {6e-6, 3e-2, 4e-7};
+    double Pc_max_rel[] = {1e7, 3e3, 2e5};
 
+    int N_rel = 2;//da scrivere anche su plot.plt
+    
+    
+    double R_rel[3][N], M_rel[3][N], Pc_rel[3][N];
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < N; ++j) {
+            dati << "\n\n(i,j) = (" << i << "," << j << ")" << endl;
+            dati << "r\tP\tm\trho" << endl;
+            Pc_rel[i][j] = Pc_min_rel[i] + ((Pc_max_rel[i] - Pc_min_rel[i]) / (N_rel - 1)) * j;
+            P = Pc_rel[i][j];
+            m = 0;
+            r = r0;
+
+            args[0] = gam[i]; args[1] = K[i];
+
+            while (P > 0) {
+                e = pow(P / (args[1] * (args[0] - 1)), 1.0 / args[0])+ P / (args[0] - 1);
+                dati << r << "\t" << P << "\t" << m << "\t" << e << endl;
+                R_rel[i][j] = r; M_rel[i][j] = m;
+                runge_kutta(r, &P, &m, h, dP, args, dm, args);
+                r += h;
+            }
+        }
+    }
+    for (int i = 0; i < 3; ++i) {
+        //dati1 << "\n\nStella " << i << endl;
+        cout << "\n\nStella Relativistica " << i << endl;
+        for (int j = 0; j < N_rel; ++j) {
+            //if(R_rel[i][j]*R0<=50 && R_rel[i][j]*R0>=3){
+                cost = pow(M_rel[i][j], 2 - gam[i]) * pow(R_rel[i][j], 3 * gam[i] - 4);
+                dati1 << Pc_rel[i][j] << "\t" << R_rel[i][j]*R0 << "\t" << M_rel[i][j] << "\t" << cost << "\t"<< endl;
+                cout << Pc_rel[i][j] << "\t" << R_rel[i][j]*R0 << "\t" << M_rel[i][j] << "\t" << cost << "\t"<< endl;
+            //}
+        }
+    }
+    
     dati.close(); dati1.close();
     return 0;
 }
@@ -62,7 +106,16 @@ int main() {
 double f(double r, double P, double m, double *fargs) {
     return - (m * pow(P / (fargs[1] * (fargs[0] - 1)), 1 / fargs[0]) / pow(r, 2)) ;
 }
-
 double g(double r, double P, double m, double *gargs) {
     return pow(r, 2) * pow(P / (gargs[1] * (gargs[0] - 1)), 1 / gargs[0]);
+}
+
+
+double dm(double r, double P, double m, double *dmargs){//assumo valgano le equazioni della volta scorsa
+    double e=pow(P / (dmargs[1] * (dmargs[0] - 1)), 1 / dmargs[0]) + P / (dmargs[0] - 1);
+    return pow(r, 2) * e;
+}
+double dP(double r, double P, double m, double *dPargs){
+    double e=pow(P / (dPargs[1] * (dPargs[0] - 1)), 1 / dPargs[0]) + P / (dPargs[0] - 1);
+    return -(P+e)*(m+pow(r, 3)*P)/(pow(r, 2)-2*m*r);
 }
--- esercitazione4/es1/plot.plt
+++ esercitazione4/es1/plot.plt
@@ -1,32 +1,62 @@
-#set term qt font "Helvetica" position 0,0
+set term qt font "Helvetica,12" position 0,0
 set datafile sep '\t'
 set grid
-stats 'dati.dat' u 1
+set format x "%2.0tx10^{%L}"
+N = 2
 
-#set multiplot layout 1,2
-set term qt 0 title "P" position 0,0
-plot 	"dati.dat" i 0 u 1:2 w l lt rgb "violet", \
-	"dati.dat" i 1 u 1:2 w l lt rgb "red", \
-	"dati.dat" i 2 u 1:2 w l
-
-set term qt 1 title "m" position 800,0
-plot 	"dati.dat" i 0 u 1:3 w l lt rgb "violet", \
-	"dati.dat" i 1 u 1:3 w l lt rgb "red", \
-	"dati.dat" i 2 u 1:3 w l
-
-set term qt 2 title "rho" position 400,350
-plot 	"dati.dat" i 0 u 1:4 w l lt rgb "violet", \
-	"dati.dat" i 1 u 1:4 w l lt rgb "red", \
-	"dati.dat" i 2 u 1:4 w l
-
-set term qt 3 title "M-R" position 400,350
+set term qt 0 title "P(r)" position 0,0
+set xlabel "r"
+set ylabel "P(r)"
 set logscale y
-set logscale x
-plot 	"dati.dat" i STATS_blocks-3 u 2:1  w lp lt rgb "violet", \
-	"dati.dat" i STATS_blocks-2 u 2:1  w lp lt rgb "red", \
-	"dati.dat" i STATS_blocks-1 u 2:1  w lp
-	
+plot "dati.dat" i 0*N u 1:2 w l lt rgb "violet" title columnheader(1),\
+	 "dati.dat" i 1*N u 1:2 w l lt rgb "red" title columnheader(1), \
+	 "dati.dat" i 2*N u 1:2 w l title columnheader(1)
 
-#plot 'file.dat' i STATS_blocks-1 usato per accedere all'ultimo blocco
+set term qt 1 title "m(r)" position 400,350
+set xlabel "r"
+set ylabel "m(r)"
+#set logscale x
+#set logscale y
+plot "dati.dat" i 0*N u 1:3 w lp lt rgb "violet" title columnheader(1),\
+	 "dati.dat" i 1*N u 1:3 w lp lt rgb "red" title columnheader(1), \
+	 "dati.dat" i 2*N u 1:3 w lp title columnheader(1)
+
+set term qt 2 title "cost(P_c)" position 0,350
+set xlabel "P_c"
+set ylabel "cost(P_c)"
+set logscale x
+plot "dati1.dat" i 0 u 1:4 w l lt rgb "violet" title columnheader(1),\
+	 "dati1.dat" i 1 u 1:4 w l lt rgb "red" title columnheader(1), \
+#	 "dati1.dat" i 2 u 1:4 w l title columnheader(1)
+
+set term qt 3 title "m(P_c)" position 400,0
+set xlabel "P_c"
+set ylabel "m(P_c)"
+set logscale x
+set logscale y
+plot "dati1.dat" i 0 u 1:3 w l lt rgb "violet" title columnheader(1),\
+	 "dati1.dat" i 1 u 1:3 w l lt rgb "red" title columnheader(1), \
+#	 "dati1.dat" i 2 u 1:3 w l title columnheader(1)
+
+set term qt 4 title "r(P_c)" position 800,0
+set xlabel "P_c"
+set ylabel "r(P_c) x R_0    [km]"
+set logscale x
+set ytics (1,3,10,50,100)
+set arrow from 4e-7,3 to 1e7,3 nohead dt "-" lc rgb "blue";
+set arrow from 4e-7,50 to 1e7,50 nohead dt "-" lc rgb "blue";
+plot "dati1.dat" i 0 u 1:2 w l lt rgb "violet" title columnheader(1),\
+	 "dati1.dat" i 1 u 1:2 w l lt rgb "red" title columnheader(1),\
+#	 "dati1.dat" i 2 u 1:2 w l title columnheader(1)
+
+reset
+set term qt 5 title "M-R" position 800,350
+set xlabel "R x R_0"
+set ylabel "M"
+set logscale y
+plot "dati1.dat" i 0 u 2:3 w l lt rgb "violet" title columnheader(1),\
+	 "dati1.dat" i 1 u 2:3 w l lt rgb "red" title columnheader(1), \
+#	 "dati1.dat" i 2 u 2:3 w l title columnheader(1)
+
 
 pause -1
