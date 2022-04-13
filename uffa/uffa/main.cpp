d6fefd3ea45e24652e436d11ac297196f60dfe5b
RandomPhile <richifurlan@gmail.com>
2022-04-13 11:28:03 +0200

    è una gaussiana

--- uffa/uffa/main.cpp
+++ uffa/uffa/main.cpp
@@ -14,60 +14,74 @@ double a(double *r, int indice, double *args);
 int main() {
     dati.open("dati.dat");
     double t , t0 = 0, h, E, Tau;
-    double sigma=3;//uguale a sqrt(1/(m*beta))
+    double sigma = 3; //uguale a sqrt(1/(m*beta))
     Tau = 1;
     double t1 = 25 * Tau;
-    int N_P = 1000001, N_M=10000;
+    int N_P = 40/0.1, N_M = 100000; //N_P PARI
     h = (t1 - t0) / ( (double) N_P);
     //printf("t\t\t\tx\t\t\ty\n");
     double r[N_P];
+    //creo la distribuziuone di velocità
+    if (N_M % 2 != 0) {
+        N_M++;
+    }
     double v[N_M];
     double a_prev[N_M];
-    
-    for (int i=0; i<12; i++) {
-        r[i]=0;
+
+    for (int i = 0; i < N_P; i++) {
+        r[i] = 0;
     }
-    
-    //creo la distribuziuone di velocità
-    if(N_M%2!=0){
-        N_P++;
-    }
-    
-    for(int i=0;i<N_M;i+=2){
-        double x1,x2;
-        x1=rand()/((double)RAND_MAX+1.0);
-        x2=rand()/((double)RAND_MAX+1.0);
-        v[i]=sigma*sqrt(-2*log(1-x2))*cos(2*M_PI*x1);
-        v[i+1]=sigma*sqrt(-2*log(1-x2))*sin(2*M_PI*x1);
+
+
+
+    for (int i = 0; i < N_M; i += 2) {
+        double x1, x2;
+        x1 = rand() / ((double)RAND_MAX + 1.0);
+        x2 = rand() / ((double)RAND_MAX + 1.0);
+        v[i] = sigma * sqrt(-2 * log(1 - x2)) * cos(2 * M_PI * x1);
+        v[i + 1] = sigma * sqrt(-2 * log(1 - x2)) * sin(2 * M_PI * x1);
+        //dati << i << "\t" << v[i] << endl;
     }
     //verifico che sia gaussiana
-    /*for(int i=0;i<N_M;i++){
-        for(int j=0;j<N_P;j++){
-            if(v[i]<-8000+(j+1) && v[i]>-8000+j){
-                r[j]++;
-                dati<<r[j]<<"\t"<<v[i]<<endl;
-                cout<<r[j]<<"\t"<<v[i]<<endl;
-                break;
+    for (double j = -20; j < 20; j += 40.0 / N_P) {
+        int cont = 0;
+        for (int i = 0; i < N_M; i++) {
+            if (v[i] < j + 40.0 / N_P && v[i] > j) {
+                cont++;
             }
         }
-    }*/
-    double v_cm=0;
-    for(int i=0;i<N_M;i++){
-        v_cm+=v[i];
+        dati << j << "\t" << cont << endl;
     }
-    v_cm/=N_M;
-    cout<<v_cm<<endl;
-    for(int i=0;i<N_M;i++){
-        v[i]-=v_cm;
-    }
-    for(int i=0;i<N_M;i++){
-        v_cm+=v[i];
-    }
-    cout<<v_cm<<endl;
-    
+    // }
+    // for(int i=0;i<N_M;i++){
+    //     for(int j=0;j<N_P;j++){
+    //         if(v[i]<-(N_P/2)+(j+0.1) && v[i]>-(N_P/2)+j){
+    //             r[j]++;
+    //             dati<<r[j]<<"\t"<<v[i]<<endl;
+    //             //cout<<r[j]<<"\t"<<v[i]<<endl;
+    //             break;
+    //         }
+    //     }
+    // }
+    // double v_cm=0;
+    // for(int i=0;i<N_M;i++){
+    //     v_cm+=v[i];
+    // }
+    // v_cm/=N_M;
+    // cout<<v_cm<<endl;
+    // for(int i=0;i<N_M;i++){
+    //     v[i]-=v_cm;
+    // }
+    // v_cm=0;
+    // for(int i=0;i<N_M;i++){
+    //     v_cm+=v[i];
+    // }
+    // v_cm/=N_M;
+    // cout<<v_cm<<endl;
+
     /*double r_mod;
     double v_mod;
-    
+
     for (int i = 0; i < 3; ++i) {
         a_prev[i] = r[i];
     }
@@ -75,7 +89,7 @@ int main() {
         t = t0 + n * h;
 
         if (vel_verlet(t, r, v, h, a_prev, a, NULL)) {printf("ERRORE");}
-        
+
         r_mod = sqrt(pow(r[0],2)+pow(r[1],2)+pow(r[2],2));
         v_mod = sqrt(pow(v[0],2)+pow(v[1],2)+pow(v[2],2));
 
@@ -87,13 +101,13 @@ int main() {
     return 0;
 }
 
-double f(double t, double x, double y, double *fargs) {
+double f(double t, double x, double y, double * fargs) {
     return y;
 }
-double g(double t, double x, double y, double *gargs) {
+double g(double t, double x, double y, double * gargs) {
     return -x;
 }
-double a(double *r, int indice, double *args) {
+double a(double * r, int indice, double * args) {
     return -r[indice];
 }
 
--- uffa/uffa/plot.plt
+++ uffa/uffa/plot.plt
@@ -2,7 +2,7 @@ set datafile sep '\t'
 set grid
 
 set term qt 0 title "Gaussian?" font "Helvetica" 
-plot "dati.dat" i 0 u 2:1 w p,\
+plot "dati.dat" i 0 u 1:2 w p,\
 
 	 
 pause-1
