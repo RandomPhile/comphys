aa253b60061443ccd3a2833ab5bf53296e628579
cris1717 <100869335+cris1717@users.noreply.github.com>
2022-04-21 16:47:47 +0200

    Add files via upload

---
+++ esercitazione4/es1_cris/eq_diff.h
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int eu_imp (double h, double *x, double *v) {
+
+   double x_n = *x;
+   double v_n = *v;
+   
+   *x = ( x_n + h*v_n ) / ( 1 + pow(h,2) );
+   *v = ( v_n - h*x_n ) / ( 1 + pow(h,2) ); 
+                                          
+   return 0;  
+ 
+}
+
+
+int trapezi(double h, double *x, double *v) {
+
+   double x_n = *x;
+   double v_n = *v;
+   
+   *x = ( x_n * ( 1 + pow(h,2)/4 ) + h*v_n ) / ( 1 + pow(h,2)/4 );
+   *v = ( v_n * ( 1 - pow(h,2)/4 ) - h*x_n ) / ( 1 + pow(h,2)/4 );
+
+   return 0;
+}
+
+int eulero_cromer (double h, double *x, double *v ){
+
+   double x_n = *x;
+   double v_n = *v;
+   
+   *v = v_n -h*x_n;
+   *x = x_n + h* ( *v );   
+
+
+   return 0;
+
+
+}
+
+
+int eulero_exp(double t, double h, double *x, double *y, double(*f)(double, double, double, double*),
+               double *fargs, double(*g)(double, double, double, double* ), double *gargs) {
+    
+    double x_n = *x;           
+   *x = *x + h * f(t, *x, *y, fargs);
+   *y = *y + h * g(t, x_n, *y, gargs);               
+               
+               
+
+
+   return 0;
+}
+
+int runge_kutta (double t, double h, double *x, double *y, double(*f)(double, double, double, double*),
+               double *fargs, double(*g)(double, double, double, double* ), double *gargs) {
+               
+               
+   double k1, k2, k3, k4;
+   double l1, l2, l3, l4;
+   
+   k1 = h * f(t, *x, *y, fargs);
+   l1 = h * g(t, *x, *y, gargs);
+   
+   k2 = h * f(t + h/2, *x + k1/2, *y + l1/2, fargs);
+   l2 = h * g(t + h/2, *x + k1/2, *y + l1/2, gargs);
+   
+   k3 = h * f(t + h/2, *x + k2/2, *y + l2/2, fargs);
+   l3 = h * g(t + h/2, *x + k2/2, *y + l2/2, gargs);
+   
+   k4 = h * f(t + h, *x + k3, *y + l3, fargs);
+   l4 = h * g(t + h, *x + k3, *y + l3, gargs);
+   
+   *x = *x + ( k1 + 2*k2 + 2*k3 + k4 )/6;
+   *y = *y + ( l1 + 2*l2 + 2*l3 + l4 )/6;            
+               
+   return 0;               
+               
+}            
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+               
+
+
+
+
+
+
+
+
