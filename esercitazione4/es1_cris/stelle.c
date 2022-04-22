a56ce09d8e108bfdeaac0c52692e5de730338c5b
cris1717 <100869335+cris1717@users.noreply.github.com>
2022-04-22 14:52:59 +0200

    codice migliorato

--- esercitazione4/es1_cris/stelle.c
+++ esercitazione4/es1_cris/stelle.c
@@ -13,18 +13,25 @@ int main () {
    P_scale = 150.1704; // MeV per fermi al cubo
    R_scale = 20.061455; //km
    M_scale = 13.6557594; //masse solari
-   double P_dim, R_dim, M_dim; // P_dim = P_var * P_scale, ecc.
    
    double r = 1e-10; 
    double h = 1e-6;
    
    int N = 20; //numero di diverse pressioni iniziali
+   
    double P_c[N][3]; //Ogni colonna è una stella, le righe sono le diverse pressioni iniziali
    double M[N][3];
    double R[N][3];
    double control[N][3]; //matrice di controllo dove calcolo M^(2 - gamma) * R^(3gamma - 4) e verifico che sia costante
    
-   double fargs[] = {0.05, 5.0/3.0}; //i parametri di g e di f sono gli stessi, uso un'unica matrice. Ogni colonna è una stella
+   double P_var; //pressione e massa che uso come variabili
+   double m_var;
+   
+   double *P, *m;
+   P = &P_var;
+   m = &m_var;
+   
+   double fargs[2]; //i parametri di g e di f sono gli stessi, uso un unico array.
    
    //Queste sono condizioni che inizializzano in modo diverso P_c in base alla stella che stiamo studiando
     
@@ -38,97 +45,107 @@ int main () {
          else {
             P_c[j][0] =  2.514*P_c[j-1][0];
             P_c[j][1] =  1.484*P_c[j-1][1];
-            P_c[j][2] =  1.543*P_c[j-1][2];
+            P_c[j][2] =  2*P_c[j-1][2]; //1.543
          }
       }
    
    /*printf("Stella 1: da %.5f a %.5f\n", P_c[0][0], P_c[N-1][0]);
    printf("Stella 2: da %.5f a %.5f\n", P_c[0][1], P_c[N-1][1]);
    printf("Stella 3: da %.5f a %.5f\n", P_c[0][2], P_c[N-1][2]);*/ //controllo gli estremi delle pressioni
-  
    
-   double P_var = P_c[0][0]; //la prima risoluzione per la prima stella la faccio "a mano" perché voglio i grafici P(r) e m(r)
-   double m_var = 0;
+   for (int i = 0; i <= 2; i++) { //con l'indice i ciclo sui diversi tipi di stelle, con j ciclo sulle condizioni iniziali
    
-   
-   double *P, *m;
-   P = &P_var;
-   m = &m_var;
-   
-   FILE *st1;
-   st1 = fopen("stella1.txt", "w");
-   fprintf(st1, "%.5f\t%.5f\t%.5f\n", r*R_scale, P_var*P_scale, m_var*M_scale);
-   
-   
-   
-   while ( P_var > 1e-9 ) { 
-   
-      runge_kutta(r, h, P, m, f, fargs, g, fargs);
-      r += h;    
-      fprintf(st1, "%.5f\t%.5f\t%.5f\n", r*R_scale, P_var*P_scale, m_var*M_scale);
-    }
-    fclose(st1);
-   
-    M[0][0] = m_var; //massa e raggio finale della prima risoluzione della prima stella
-    R[0][0] = r;
-    control[0][0] = pow(M[0][0], 2 - fargs[1])*pow(R[0][0],3*fargs[1] - 4);
+      if ( i == 0) { //risolvo per la prima stella; per la prima stella stampo anche i risultati in modo da eseguire un plot di
+                     //P(r) e m(r)
+                                                  
+         fargs[0] = 0.05;
+         fargs[1] = 5.0/3.0;
+         
+         char filename[15]; //stringa a cui sprintf assegnerà un nome diverso ad ogni ciclo
+         FILE *st; //puntatore che uso per stampare i risultati su un file.txt
+                  
+         for (int j = 0; j < N ; j++ ) {
+         
+            r = 1e-10;
+            P_var = P_c[j][0];
+            m_var = 0;
+            
+            sprintf(filename, "stella%d.txt", j+1); //in modo da avere stella1.txt, ..., stellaN.txt
+            st = fopen( filename, "w");
+            fprintf(st, "%.5f\t%.5f\t%.5f\n", r*R_scale, P_var*P_scale, m_var*M_scale);
     
-    //ora risolvo anche per gli altri valori di Pc, sempre per la stessa stella, e quindi stella colonna 0
-    
-    for (int j = 1; j < N ; j++ ) {
-    
-    r = 1e-10;
-    P_var = P_c[j][0];
-    m_var = 0;
-    
-       while (P_var > 1e-9) {
+            while (P_var > 1e-9) {
        
-          runge_kutta(r, h, P, m, f, fargs, g, fargs);
-          r += h;  
+               runge_kutta(r, h, P, m, f, fargs, g, fargs);
+               r += h;  
+               fprintf(st, "%.5f\t%.5f\t%.5f\n", r*R_scale, P_var*P_scale, m_var*M_scale);
               
-       }
-    
-    M[j][0] = m_var;
-    R[j][0] = r;
-    control[j][0] = pow(M[j][0], 2 - fargs[1])*pow(R[j][0],3*fargs[1] - 4);
+            }
+            fclose(st);
+         
+            M[j][i] = m_var;
+            R[j][i] = r;
+            control[j][i] = pow(M[j][i], 2 - fargs[1])*pow(R[j][i],3*fargs[1] - 4);
+         
+         }   
       
-    }
-    
-   //In questo modo ottengo i punti M(R) per la prima stella. Ora risolvo per le altre due
+      }
    
-   for (int i = 1; i <= 2; i++) {
-   
-      if ( i == 1) {           //inizializzo gli argomenti in base a quale stella sto risolvendo, passaggio evitabile ma rende il codice più
-                               //leggibile
+      else if ( i == 1) {           //risolvo per la seconda stella
+                                     
          fargs[0] = 0.1;
-         fargs[1] = 4.0/3.0;     
+         fargs[1] = 4.0/3.0;    
+         
+         for (int j = 0; j < N; j++) { 
+      
+            r = 1e-10;
+            P_var = P_c[j][i];
+            m_var = 0;
+         
+            while (P_var > 1e-9) {
+         
+               runge_kutta(r, h, P, m, f, fargs, g, fargs);
+               r += h;
+                    
+            }
+         
+            M[j][i] = m_var;
+            R[j][i] = r;
+            control[j][i] = pow(M[j][i], 2 - fargs[1])*pow(R[j][i],3*fargs[1] - 4);      
+         }    
       }
       
-      else {
+      else  {         
+                        //risolvo per la terza stella
          fargs[0] = 0.01;      
          fargs[1] = 2.54;
-      }
       
-      for (int j = 0; j < N; j++) {
+         
+         for (int j = 0; j < N; j++) { 
       
-         r = 1e-10;
-         P_var = P_c[j][i];
-         m_var = 0;
+      
+            r = 1e-10;
+            P_var = P_c[j][i];
+            m_var = 0;
          
-         while (P_var > 1e-9) {
+            while (P_var > 1e-7) {
          
-            runge_kutta(r, h, P, m, f, fargs, g, fargs);
-            r += h;
+               runge_kutta(r, h, P, m, f, fargs, g, fargs);
+               r += h;
                     
-         }
-         
-         M[j][i] = m_var;
-         R[j][i] = r;
-         control[j][i] = pow(M[j][i], 2 - fargs[1])*pow(R[j][i],3*fargs[1] - 4);      
+            }
+                 
+            M[j][i] = m_var;
+            R[j][i] = r;
+            control[j][i] = pow(M[j][i], 2 - fargs[1])*pow(R[j][i],3*fargs[1] - 4);      
+         }     
       
-      }   
+      }    
+         
    }
    
+   
+   //Ho risolto tutto, ora stampo il file con massa totale in funzione di raggio totale, ossia gli elementi di M[j][i] e R[j][i]
    FILE *mr;
    mr = fopen("massa_raggio.txt", "w");
    
