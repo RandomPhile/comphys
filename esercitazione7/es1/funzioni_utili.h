34369bdf2c28a8d2e3fd43d1261b436c12d41450
RandomPhile <richifurlan@gmail.com>
2022-04-27 15:51:46 +0200

    ok?

--- esercitazione7/es1/funzioni_utili.h
+++ esercitazione7/es1/funzioni_utili.h
@@ -24,16 +24,8 @@ long double pow1(long double base, int esp) {//funzione esponenziale creata per 
     }
     return ris;
 }
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-void gauss_distr(double *v, double sigma, int N_Mol) {
-    for (int i = 0; i < 3 * N_Mol; i += 2) {//tutte 3N coordinate
-=======
->>>>>>> Stashed changes
 void gauss_distr(double *v, double sigma, int N_Mol) { //crea distribuzione gaussiana per un vettore in ingresso
     for (int i = 0; i < 3 * N_Mol; i += 2) {
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
         double x1, x2;
         x1 = rand() / ((double)RAND_MAX + 1.0);
         x2 = rand() / ((double)RAND_MAX + 1.0);
@@ -64,18 +56,6 @@ void compila_matr(double *r, double *a_prev, int N_Mol,
     }
 }
 void compila_matr(double *r, double *a_prev, int N_Mol,
-<<<<<<< HEAD
-                  void (*f)(double*, double*, long double*, int),
-                  double *args) {
-    for (int j = 0; j < N_Mol; j++) {
-        long double F[3];
-        f(r, args, F, j);//aggiorna F[3]
-        for (int i = 3 * j; i < 3 * j + 3; ++i) {
-            a_prev[i] = F[i - 3 * j];
-        }
-    }
-}
-=======
                    void (*f)(double*, double*, long double*, int),
                    double *args) {//assegna alla matrice a_prev il valore di accelerazione imposto dal potenziale versione long double
      for (int j = 0; j < N_Mol; j++) {
@@ -86,7 +66,6 @@ void compila_matr(double *r, double *a_prev, int N_Mol,
          }
      }
  }
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
 void copia_vett(double *r, int N_Mol, double *v) {
     for (int j = 0; j < 3 * N_Mol; j++) {
         r[j] = v[j];
@@ -94,8 +73,8 @@ void copia_vett(double *r, int N_Mol, double *v) {
 }
 void set_vcm0(double *v, int N_Mol) {//imposta la velocita media a zero
     double v_cm[] = {0, 0, 0};
-    for (int j = 0; j < 3; j++) {//per 3 direzioni
-        for (int i = 0; i < N_Mol; i++) {//per N_mol particelle
+    for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < N_Mol; i++) {
             v_cm[j] += v[i + 3 * j];
         }
     }
@@ -111,21 +90,6 @@ void set_vcm0(double *v, int N_Mol) {//imposta la velocita media a zero
 void crea_reticolo(int N_mol, double L, double *r) {
     double x, y, z, b;
     b = L / cbrt(N_mol);
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-    int k = 0; //contatore della posizione
-    for (x = 0; x < L - b; x += b) {
-        for (y = 0; y < L - b; y += b) {
-            for (z = 0; z < L - b; z += b) {
-                r[k] = x;
-                r[k + 1] = y;
-                r[k + 2] = z;
-                k += 3;
-                if (k > 3 * N_mol)
-                    break;
-=======
->>>>>>> Stashed changes
     int k = 0; //contatore della particella
     for (x = 0; x < L; x += b) {
         for (y = 0; y < L; y += b) {
@@ -136,34 +100,19 @@ void crea_reticolo(int N_mol, double L, double *r) {
                 //cout << r[k] << "\t" << r[k + 1] << "\t" << r[k + 2] << "\t" << k/3 << endl;
                 dati << r[k] << "\t" << r[k + 1] << "\t" << r[k + 2] << "\t" << k/3 << endl;
                 k+=3;
-<<<<<<< Updated upstream
-=======
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
->>>>>>> Stashed changes
             }
         }
     }
+    dati<<"\n\n";
 }
-void stampa_reticolo(int N_mol, double *r) {
-    for (int i = 0; i < 3 * N_mol; i += 3) {
-        dati << r[i] << "\t" << r[i + 1] << "\t" << r[i + 2] << "\t" << i / 3<< endl;
+void stampa_reticolo(int N_mol, double L, double *r) {
+    for(int i=0; i<3*N_mol;i+=3){
+        dati<<r[i]<<"\t"<<r[i+1]<<"\t"<<r[i+2]<<endl;
     }
-    dati << "\n\n";
 }
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-void cond_bordo(double *r, int N_mol, double L) {
-    /*se il valore di una coordinata è maggiore di L,
-      trasla la particella di -L*/
-    for (int j = 0; j < 3 * N_mol; j++) {
-        r[j] = r[j] - L * rint(r[j] / L);
-=======
->>>>>>> Stashed changes
 void cond_bordo(double *r,int N_mol, double L) {//impone condizioni di bordo periodiche su tutte le particelle
     for (int j = 0; j < 3*N_mol; j++) {
         r[j]=r[j]-L*rint(r[j]/L);
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
     }
 }
 
--- esercitazione7/es1/main.cpp
+++ esercitazione7/es1/main.cpp
@@ -12,92 +12,42 @@ double f(double t, double x, double y, double *fargs);
 double g(double t, double x, double y, double *gargs);
 double a(double *r, int indice, double *args);
 
-<<<<<<< Updated upstream
 int N_mol = pow1((double)6, 3);
-=======
-<<<<<<< HEAD
-int N_mol = pow1((double)3, 3);//numero di particelle
-=======
-int N_mol = pow1((double)6, 3);
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
->>>>>>> Stashed changes
 
 int main() {
     dati.open("dati.dat");
     double t = 0, h, E, K, T, V;
-<<<<<<< Updated upstream
     double t0 = 0, t1 = 20;
 
-=======
-<<<<<<< HEAD
-    double t0 = 0, t1 = 25;
-
-    int N = 1000;//numero di passi temporali
-    h = (t1 - t0) / ( (double) N);//larghezza passo temporale
-    double rho = 1e-2; //densita del campione da studiare
-=======
-    double t0 = 0, t1 = 20;
-
->>>>>>> Stashed changes
     int N = 100;
     h = (t1 - t0) / ( (double) N);
     double rho = 1e-2; //fisso la densita del campione da studiare
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
     double eps, sigma, L, distanza_interaz;
     L = cbrt(N_mol / rho);
-    distanza_interaz = L / 2;
+    distanza_interaz = L/2;
+    //cout<<L<<endl;
 
-<<<<<<< Updated upstream
     double var_ad[] = {eps=1e-6, sigma=1e-1, distanza_interaz, L};
-=======
-<<<<<<< HEAD
-    double var_ad[] = {eps = 1e-6, sigma = 1e-6, distanza_interaz, L};
-=======
-    double var_ad[] = {eps=1e-6, sigma=1e-1, distanza_interaz, L};
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
->>>>>>> Stashed changes
 
-    double r[3 * N_mol], v[3 * N_mol];//vettori 3N dimensionali posizione e velocità
-    double r_mod, v_mod;//scalari modulo
+    double r[3 * N_mol], v[3 * N_mol];
+    double r_mod, v_mod;
 
     r_mod = sqrt(r[0] * r[0] + r[0 + 1] * r[0 + 1] + r[0 + 2] * r[0 + 2]);
     v_mod = sqrt(v[0] * v[0] + v[0 + 1] * v[0 + 1] + v[0 + 2] * v[0 + 2]);
 
-<<<<<<< Updated upstream
     crea_reticolo(N_mol, L, r);//crea il reticolo
     
     gauss_distr(v, 1.1, N_mol);//crea la distribuzione gaussiana di velocita
     set_vcm0(v, N_mol);//impone la velocita media delle particelle a 0
 
     double a_prev[3 * N_mol];
-=======
-<<<<<<< HEAD
-    crea_reticolo(N_mol, L, r);//dispone i valori di posizione lungo un reticolo
-    stampa_reticolo(N_mol, r);//salva nel file dati i valori delle posizioni
-
-    gauss_distr(v, 1.2, N_mol);//aggiorno le velocità secondo una distribuzione gaussiana
-    set_vcm0(v, N_mol);
-
-    double a_prev[3 * N_mol];
-    compila_matr(r, a_prev, N_mol, fLJ, var_ad);//inizializzo accelerazioni come date dal potenziale
-
-=======
-    crea_reticolo(N_mol, L, r);//crea il reticolo
-    
-    gauss_distr(v, 1.1, N_mol);//crea la distribuzione gaussiana di velocita
-    set_vcm0(v, N_mol);//impone la velocita media delle particelle a 0
-
-    double a_prev[3 * N_mol];
->>>>>>> Stashed changes
     compila_matr(r, a_prev, N_mol, fLJ, var_ad);//inizializzo accelerazioni come date dal potenziale al tempo 0
     
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
     E = 0; K = 0; V = 0;
     for (int n = 0; n < N; ++n) {//passi temporali
         K = K * (n + 1); //energia cinetica @t
         V = V * (n + 1); //energia potenziale @t
         T = 0;//temp @t
-<<<<<<< Updated upstream
         
         t = t0 + n * h;//imposto il tempo
         
@@ -107,40 +57,6 @@ int main() {
             
             //trovo il modulo della particella corrente per trovare il suo contributo alla energia cinetica
             v_mod = sqrt(v[j] * v[j] + v[j + 1] * v[j + 1] + v[j + 2] * v[j + 2]);
-=======
-<<<<<<< HEAD
-        //           r_x           r_y                   r_z
-        r_mod = sqrt(r[0] * r[0] + r[0 + 1] * r[0 + 1] + r[0 + 2] * r[0 + 2]);
-        v_mod = sqrt(v[0] * v[0] + v[0 + 1] * v[0 + 1] + v[0 + 2] * v[0 + 2]);
-
-        for (int j = 0; j < N_mol; ++j) {//per N particelle j
-            t = t0 + n * h;//incremento tempo
-
-            //aggiorno posizione e velocità con vel_verlet:
-            if (vel_verlet(t, r, v, h, j, a_prev, fLJ, var_ad)) {printf("ERRORE");}
-
-            v_mod = sqrt(v[j] * v[j] + v[j + 1] * v[j + 1] + v[j + 2] * v[j + 2]);
-
-
-            cond_bordo(r, N_mol, L);//aggiorno la posizione spostando le particelle dentro alla scatola
-            for (int i = 0; i < j; ++i) {//attenzione che è usata la distanza effettiva invece della distanza della prima copia vicina
-                double x_ij = r[3 * i] - r[3 * j];
-                double y_ij = r[3 * i + 1] - r[3 * j + 1];
-                double z_ij = r[3 * i + 2] - r[3 * j + 2];
-                V += VLJ(sqrt(x_ij * x_ij + y_ij * y_ij + z_ij * z_ij), var_ad);
-            }
-
-=======
-        
-        t = t0 + n * h;//imposto il tempo
-        
-        for (int j = 0; j < N_mol; ++j) {//particelle
-            
-            if (vel_verlet(t, r, v, h, j, a_prev, fLJ, var_ad)) {printf("ERRORE");}//esegue l'algoritmo di velocity verlet
-            
-            //trovo il modulo della particella corrente per trovare il suo contributo alla energia cinetica
-            v_mod = sqrt(v[j] * v[j] + v[j + 1] * v[j + 1] + v[j + 2] * v[j + 2]);
->>>>>>> Stashed changes
             
             //impongo condizioni di bordo periodiche
             cond_bordo(r, N_mol, L);
@@ -152,10 +68,6 @@ int main() {
                 double z_ij=r[3 * i + 2] - r[3 * j + 2];
                 V += VLJ(sqrt(x_ij * x_ij + y_ij * y_ij + z_ij * z_ij), var_ad);
             }
-<<<<<<< Updated upstream
-=======
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
->>>>>>> Stashed changes
             K += 0.5 * v_mod * v_mod;
             
         }
@@ -168,15 +80,10 @@ int main() {
         dati << t  << "\t" << E << "\t" << K << "\t" << V << "\t" << T << endl;
         //cout << t  << "\t" << E << "\t" << K << "\t" << V << "\t" << T << endl;
     }
-<<<<<<< HEAD
-    dati << "\n\n";
-    stampa_reticolo(N_mol, r);
-=======
     dati<<"\n\n";
         
     //stampo il reticolo in posizione finale
     stampa_reticolo(N_mol, L, r);
->>>>>>> a14900cb9df682e2da9c140b85653d8582dcf285
     dati.close();
     return 0;
 }
@@ -190,4 +97,3 @@ double g(double t, double x, double y, double *gargs) {
 double a(double *r, int indice, double *args) {
     return -r[indice];
 }
-
