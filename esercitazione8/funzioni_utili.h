d520f8b3458f30b336e53c9093468b9f5b0b5ae5
elericon <100796314+elericon@users.noreply.github.com>
2022-05-08 14:46:31 +0200

    correzioni ma ancora qualcosa non torna

--- esercitazione8/funzioni_utili.h
+++ esercitazione8/funzioni_utili.h
@@ -17,11 +17,19 @@ double pow1(double base, int esp) {//funzione esponenziale creata per non usare 
     }
     return ris;
 }
-void plot_hist(double *v, int N_mol, double L) {
+void plot_hist(double *v, int N_mol) {
     //usare N grande
-    double v_max = L;
-    double v_min = 0;
+    double v_max = 0;
+    double v_min = 1e50;
     int N_v = 1 + 3.322 * log(N_mol); //Sturge's Rule
+    for (int i = 0; i < N_mol; ++i){
+        if(v[i]<v_min){
+            v_min=v[i];
+        }
+        if(v[i]>v_max){
+            v_max=v[i];
+        }
+    }
     double v_step = (v_max - v_min) / N_v;
     double bins;
     double f_v;
@@ -41,11 +49,10 @@ void plot_hist(double *v, int N_mol, double L) {
 double integrale_montecarlo(double (*f)(double, double*), void (*distr)(double*, int, double*), double errore, double *args_f, double *args_d){//versione distrib uniforme
     //args_d: 0 inizio, 1 fine, 2 sigma, 3 mu
     
-    int N=rint(pow1(args_d[2]/errore,2)+0.5);//
+    int N=rint(pow1(1/errore,2)+0.5);//
     double ris=0;
     double x[N];
     distr(x, N, args_d);
-    plot_hist(x,N,100);
     for (int i = 1; i <= N; ++i){
         ris+=f(x[i],args_f);
     }
@@ -62,6 +69,7 @@ double integrale_montecarlo(double (*f)(double, double*), double *args_f,
     double ris=0;
     double x[N];
     distr(x, N, args_d);
+    //plot_hist(x,N);
     for (int i = 0; i <  N; ++i){
         ris+=f(x[i],args_f)/g(x[i],args_g);
     }
@@ -70,17 +78,25 @@ double integrale_montecarlo(double (*f)(double, double*), double *args_f,
 }
 
 
-double sin_n(double x, double *par){//primo parametro il coefficiente alfa interno al seno, secondo parametro l'esponente di x, terzo parametro il valore di cui è elevato il tutto
+double sin_n(double x, double *par){//0 alfa interno al seno, 1 l'esponente di x, 2 il valore di cui è elevato il tutto
     double ris=pow1(sin(x*par[0]),2)*pow1(x, par[1]);
     return pow1(ris, par[2]);
 }
-double exp_n(double x, double *par){//primo parametro il coefficiente alfa interno all'esponenziale, secondo parametro l'esponente di x, terzo parametro il valore di cui è elevato il tutto
+double exp_n(double x, double *par){//0 alfa interno all'esponenziale, 1 l'esponente di x, 2 il valore di cui è elevato il tutto
     double ris=exp(x*par[0])*pow1(x, par[1]);
     return pow1(ris, par[2]);
 }
 double gauss(double x, double *par){//primo parametro sigma, secondo parametro mu
-    double ris=exp((x-par[1])*(x-par[1])/(2*par[0]*par[0]));
+    double ris=exp(-(x-par[1])*(x-par[1])/(2*par[0]*par[0]));
     return ris;
+}
+double g_prof(double x, double *par){
+    if(x>=3){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
 #endif /* funzioni_utili_h */
