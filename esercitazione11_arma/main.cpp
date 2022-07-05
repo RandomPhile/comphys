8a7779f5fc3a213ee46cd716732db5c5aae22ebb
elericon <100796314+elericon@users.noreply.github.com>
2022-07-05 12:08:14 +0200

    montecsarlo

--- esercitazione11_arma/main.cpp
+++ esercitazione11_arma/main.cpp
@@ -36,6 +36,7 @@ double mod(cube &r, double riga, double colonna);
 double V_LJ(double r, double L);
 void MRT2(mat &r, double *V, double *W, int N, double Delta, double T, double L, cube &dr, cube &dr_n);
 double dV_LJ(double r, double L);
+void calcola_osservabili(rowvec rho, rowvec passi_eq, int caso_min, int N_t, int q, double T_req);
 // -----------------------------------------FUNZIONI----------------------------FUNZIONI------------------------------FUNZIONI-----------------------------------------------
 // -----------------------------------------FUNZIONI----------------------------FUNZIONI------------------------------FUNZIONI-----------------------------------------------
 
@@ -61,127 +62,19 @@ int main() {
     // passi_eq *= 10;
     // passi_eq.fill(2e4);
     int caso_min = 4;//mettere -1 per avere P(rho)
-    int q = 1e4;//numero punti in piu rispetto al tempo di equilibrazione 1.8e6+1.8e5
+    int q = 1e5;//numero punti in piu rispetto al tempo di equilibrazione 1.8e6+1.8e5
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-
-
-
-    // int caso_max;
-    // if (caso_min == -1) {
-    //     caso_max = rho.size();
-    // } else {
-    //     caso_max = caso_min + 1;
-    // }
-    
-    // //###################################################
-    // int reticolo   = log2(M);
-    // gnuplot.open("gnuplot.dat");
-    // gnuplot << caso_min << endl;
-    // gnuplot.close();
-
-    // risultati.open("risultati.dat");
-    // dati.open("dati.dat");
-    // dati_blocking.open("dati_blocking.dat");
-    // int start;
-    // if(caso_min == -1){
-    //     start = caso_min + 1;
-    // }
-    // else{
-    //     start = caso_min;
-    // }
-
-    // mat r(N,3);
-    // cube dr(N,N,3), dr_n(N,N,3);
-
-    // for (int caso = start; caso < caso_max; ++caso) {
-
-    //     N_t = passi_eq(caso) + q;//faccio fare 10000 passi dopo l'equilibrazione per avere dei risultati carini
-
-    //     double L = cbrt(N / rho(caso));
-    //     double Delta;
-
-    //     Delta = Calcola_Delta(L, rho, caso);
-
-    //     crea_reticolo(r, L);//creo il reticolo iniziale
-
-    //     for (int i = 0; i < N; ++i){//creo il cubo di distanze relative alla posizione iniziale
-    //         for (int j = i + 1; j < N; ++j){
-    //             for (int k = 0; k < 3; ++k){
-    //                 dr(i,j,k) = r(i,k) - r(j,k);
-    //                 dr(i,j,k) -= L * rint(dr(i,j,k)/L);//sposto in [-L/2,+L/2]
-    //             }
-    //         }
-    //     }
-
-    //     double P = 0;
-    //     double E_c = 3./2.*N*T_req;
-    //     double E = 0;
-    //     double V = 0, W = 0;
-    //     double V_m = 0, W_m = 0, P_m = 0, E_m = 0;
-    //     double var_P = 0, var_E = 0;
-
-    //     for (int i = 0; i < N_t; ++i) {//passi
-    //         if(i > passi_eq(caso) + 1){//calcola le medie
-    //             V_m = V_m * (i - passi_eq(caso) - 1.0);
-    //             W_m = W_m * (i - passi_eq(caso) - 1.0);
-    //             var_P = var_P * (i - passi_eq(caso) - 1.0);
-    //             var_E = var_E * (i - passi_eq(caso) - 1.0);
-    //         }
-    //         dr_n = dr;
-    //         MRT2(r, &V, &W, N, Delta, T_req, L, dr, dr_n);//metropolis
-
-    //         E = E_c + V;
-    //         P = (1 + W / (3.0 * T_req));
-
-    //         if(i > passi_eq(caso)){//tempo di equilibrazione e calcola le medie
-    //             V_m = (V_m + V) / (i - passi_eq(caso));
-    //             W_m = (W_m + W) / (i - passi_eq(caso));
-    //             E_m = E_c + V_m;
-                
-    //             P_m = (1 + W_m / (3.0 * T_req)); //P su rho*k_B*T_req
-    //             var_P = (var_P + (P - P_m) * (P - P_m)) / (i - passi_eq(caso));//calcolo la varianza "ordinaria"
-    //             var_E = (var_E + (E - E_m) * (E - E_m)) / (i - passi_eq(caso));//calcolo la varianza "ordinaria"
-    //         }
-
-    //         if (caso_min != -1) {
-    //             dati << i << "\t" << V_m << "\t" << P_m << "\t" << E_m  << "\t" << V << "\t" << P << "\t" << sqrt(var_P) << "\t" << E << "\t" << sqrt(var_E) << endl;
-    //             if(i > passi_eq(caso)){
-    //                 dati_blocking << P << "\t" << E << endl;
-    //             }
-    //         }
-
-    //         if(i == (int)(N_t / 2) && caso_min != -1){
-    //             cout<<"Sono a metà dei cicli montecarlo, dai che ce la faccio"<<endl;
-    //         }
-    //     }
-
-    //     cout << "Densità = "<< rho(caso) << "\t" <<"Pressione = " << P_m << "\t" <<"Energia = " << E_m << endl;
-    //     if (caso_min == -1) {
-    //         dati << rho(caso) << "\t" << P << "\t" << sqrt(var_P) << endl;
-    //     }
-    //     else{//calcolo della gdr
-    //         risultati << rho(caso) << "\t" << P_m << "\t" << sqrt(var_P) << endl;
-    //        // gdr_funz(r, L, rho(caso), N_b);
-    //     }
-    //     cout << "Ho accettato il " << (double)numero_accettati / (double)numero_proposti * 100 << "%. I passi totali erano " << N_t << "\n\n";
-    // }
-    // dati_blocking.close();
-    // dati.close();
-    // risultati.close();
-
-
-
-
-// -------------------------------------------PLOT---------------------------------PLOT-----------------------------------PLOT-----------------------------------------------
-// -------------------------------------------PLOT---------------------------------PLOT-----------------------------------PLOT-----------------------------------------------
+    // calcola_osservabili(rho, passi_eq, caso_min, N_t, q, T_req);
 
     // plot_pressioni_energie(caso_min);//plot pressioni ed energie
 
     // blocking(passi_eq(caso_min)+q);//plot blocking
 
-    bootstrap(passi_eq(caso_min)+q);//plot con bootstrap blocking
+    // bootstrap(passi_eq(caso_min)+q);//plot con bootstrap blocking
+
+    bsp_blocking(passi_eq(caso_min)+q);
 
     return 0;
 }
@@ -326,6 +219,110 @@ void MRT2(mat &r, double *V, double *W, int N, double Delta, double T, double L,
         }
         *W/=N;
     }
+}
+void calcola_osservabili(rowvec rho, rowvec passi_eq, int caso_min, int N_t, int q, double T_req){
+    int caso_max;
+    if (caso_min == -1) {
+        caso_max = rho.size();
+    } else {
+        caso_max = caso_min + 1;
+    }
+    
+    //###################################################
+    int reticolo   = log2(M);
+    gnuplot.open("gnuplot.dat");
+    gnuplot << caso_min << endl;
+    gnuplot.close();
+
+    risultati.open("risultati.dat");
+    dati.open("dati.dat");
+    dati_blocking.open("dati_blocking.dat");
+    int start;
+    if(caso_min == -1){
+        start = caso_min + 1;
+    }
+    else{
+        start = caso_min;
+    }
+
+    mat r(N,3);
+    cube dr(N,N,3), dr_n(N,N,3);
+
+    for (int caso = start; caso < caso_max; ++caso) {
+
+        N_t = passi_eq(caso) + q;//faccio fare 10000 passi dopo l'equilibrazione per avere dei risultati carini
+
+        double L = cbrt(N / rho(caso));
+        double Delta;
+
+        Delta = Calcola_Delta(L, rho, caso);
+
+        crea_reticolo(r, L);//creo il reticolo iniziale
+
+        for (int i = 0; i < N; ++i){//creo il cubo di distanze relative alla posizione iniziale
+            for (int j = i + 1; j < N; ++j){
+                for (int k = 0; k < 3; ++k){
+                    dr(i,j,k) = r(i,k) - r(j,k);
+                    dr(i,j,k) -= L * rint(dr(i,j,k)/L);//sposto in [-L/2,+L/2]
+                }
+            }
+        }
+
+        double P = 0;
+        double E_c = 3./2.*N*T_req;
+        double E = 0;
+        double V = 0, W = 0;
+        double V_m = 0, W_m = 0, P_m = 0, E_m = 0;
+        double var_P = 0, var_E = 0;
+
+        for (int i = 0; i < N_t; ++i) {//passi
+            if(i > passi_eq(caso) + 1){//calcola le medie
+                V_m = V_m * (i - passi_eq(caso) - 1.0);
+                W_m = W_m * (i - passi_eq(caso) - 1.0);
+                var_P = var_P * (i - passi_eq(caso) - 1.0);
+                var_E = var_E * (i - passi_eq(caso) - 1.0);
+            }
+            dr_n = dr;
+            MRT2(r, &V, &W, N, Delta, T_req, L, dr, dr_n);//metropolis
+
+            E = E_c + V;
+            P = (1 + W / (3.0 * T_req));
+
+            if(i > passi_eq(caso)){//tempo di equilibrazione e calcola le medie
+                V_m = (V_m + V) / (i - passi_eq(caso));
+                W_m = (W_m + W) / (i - passi_eq(caso));
+                E_m = E_c + V_m;
+                
+                P_m = (1 + W_m / (3.0 * T_req)); //P su rho*k_B*T_req
+                var_P = (var_P + (P - P_m) * (P - P_m)) / (i - passi_eq(caso));//calcolo la varianza "ordinaria"
+                var_E = (var_E + (E - E_m) * (E - E_m)) / (i - passi_eq(caso));//calcolo la varianza "ordinaria"
+            }
+
+            if (caso_min != -1) {
+                dati << i << "\t" << V_m << "\t" << P_m << "\t" << E_m  << "\t" << V << "\t" << P << "\t" << sqrt(var_P) << "\t" << E << "\t" << sqrt(var_E) << endl;
+                if(i > passi_eq(caso)){
+                    dati_blocking << P << "\t" << E << endl;
+                }
+            }
+
+            if(i == (int)(N_t / 2) && caso_min != -1){
+                cout<<"Sono a metà dei cicli montecarlo, dai che ce la faccio"<<endl;
+            }
+        }
+
+        cout << "Densità = "<< rho(caso) << "\t" <<"Pressione = " << P_m << "\t" <<"Energia = " << E_m << endl;
+        if (caso_min == -1) {
+            dati << rho(caso) << "\t" << P << "\t" << sqrt(var_P) << endl;
+        }
+        else{//calcolo della gdr
+            risultati << rho(caso) << "\t" << P_m << "\t" << sqrt(var_P) << endl;
+           // gdr_funz(r, L, rho(caso), N_b);
+        }
+        cout << "Ho accettato il " << (double)numero_accettati / (double)numero_proposti * 100 << "%. I passi totali erano " << N_t << "\n\n";
+    }
+    dati_blocking.close();
+    dati.close();
+    risultati.close();
 }
 // -----------------------------------------FUNZIONI----------------------------FUNZIONI------------------------------FUNZIONI-----------------------------------------------
 // -----------------------------------------FUNZIONI----------------------------FUNZIONI------------------------------FUNZIONI-----------------------------------------------
